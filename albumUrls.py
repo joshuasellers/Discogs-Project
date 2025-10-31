fd2d5b4fde398e4ec708964680ace27ec759c3e4
Josh Sellers <joshuasellersd@gmail.com>
2025-10-31 13:43:55 -0700

    basic image to discog title serach

---
+++ albumUrls.py
@@ -0,0 +1,2 @@
+# test albums (obtained from github urls)
+sgt = "https://github.com/joshuasellers/Discogs-Project/blob/main/albums/sgt.%20pepper's.jpeg?raw=true"
--- discogstest.py
+++ discogstest.py
@@ -1,12 +1,13 @@
 import tokens
-import discogs_client
+import albumUrls
+import discogs_client  # https://github.com/joalla/discogs_client
 import serpapi  # potential free alternative: https://github.com/RMNCLDYO/Google-Reverse-Image-Search
 
 client = serpapi.Client(api_key=tokens.serpapi_key)
-results = client.search(engine="google_reverse_image", image_url="https://example.com/image.jpg")
-print(results)
+results = client.search(engine="google_reverse_image", image_url=albumUrls.sgt)
+title = results['image_results'][0]['title']
 
 d = discogs_client.Client('VinylImageReadingProject/0.1', user_token=tokens.discogs_token)
 
-results = d.search('Stockholm By Night', type='release')
-print(results.pages)
+results = d.search(title, type='release')
+print(results[0].artists[0])
