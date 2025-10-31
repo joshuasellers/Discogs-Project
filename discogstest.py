c26f95ed567c2c78c252c7e71e6cd84f7fadd0c5
Josh Sellers <joshuasellersd@gmail.com>
2025-10-31 13:31:35 -0700

    adding reverse image search code

--- discogstest.py
+++ discogstest.py
@@ -1,7 +1,12 @@
 import tokens
 import discogs_client
+import serpapi  # potential free alternative: https://github.com/RMNCLDYO/Google-Reverse-Image-Search
 
-d = discogs_client.Client('ExampleApplication/0.1', user_token=tokens.token)
+client = serpapi.Client(api_key=tokens.serpapi_key)
+results = client.search(engine="google_reverse_image", image_url="https://example.com/image.jpg")
+print(results)
+
+d = discogs_client.Client('VinylImageReadingProject/0.1', user_token=tokens.discogs_token)
 
 results = d.search('Stockholm By Night', type='release')
 print(results.pages)
