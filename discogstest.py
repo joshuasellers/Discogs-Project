061221ddff31794f3a3ce1362faf0e604bd9dbcf
Josh Sellers <joshuasellersd@gmail.com>
2025-11-04 16:44:09 -0800

    Update discogstest.py

--- discogstest.py
+++ discogstest.py
@@ -7,7 +7,7 @@ from dropbox import DropboxOAuth2FlowNoRedirect
 
 
 def dropbox(app, secret):
-    auth_flow = DropboxOAuth2FlowNoRedirect(APP_KEY, APP_SECRET)
+    auth_flow = DropboxOAuth2FlowNoRedirect(app, secret)
 
     authorize_url = auth_flow.start()
     print("1. Go to: " + authorize_url)
