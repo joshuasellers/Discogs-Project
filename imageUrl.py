7947442efc5823fa62ce8225052b9e4efc573206
Josh Sellers <joshuasellersd@gmail.com>
2025-11-11 20:51:49 -0800

    cleaned up file and added way to get all urls

--- imageUrl.py
+++ imageUrl.py
@@ -1,9 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import urllib
 
-"""
+
 def iterate_nested_json(obj):
+    """
+    helper function to print the contents of a nested JSON
+    :param obj:
+    :return: none
+    """
     if isinstance(obj, dict):
         for key, value in obj.items():
             print(f"Key: {key}, Value: {value}")
@@ -14,18 +20,43 @@ def iterate_nested_json(obj):
     else:
         # Handle primitive types (strings, numbers, booleans)
         pass
-"""
 
 
-url = "https://github.com/joshuasellers/Discogs-Project/blob/main/albums/sgt.%20pepper's.jpeg"
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
+def get_album_links():
+    """
+    returns all of the links to the album images in this project
+    as hosted on github.
+    :return: list of album art urls
+    """
+    url = "https://github.com/joshuasellers/Discogs-Project/blob/main/albums/"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    a = soup.find_all('script')
+    albums = list(filter(lambda x: "name" in str(x) and "path" in str(x), a))[0]
+    json_albums = json.loads(albums.string)
+    album_links = []
+    if json_albums.get('payload').get('tree').get('items'):
+        for item in json_albums.get('payload').get('tree').get('items'):
+            album_links.append(url + urllib.parse.quote(item['name']))
+    print(album_links)
+    return album_links
 
-# Find the image tag (adjust selector based on the website's HTML)
-scripts = soup.find_all('script')
-image_tag = list(filter(lambda x: "displayUrl" in str(x), scripts))[0]
-json_data = json.loads(image_tag.string)
-if json_data.get('payload').get('blob'):
-    print(f"Image URL found: {json_data.get('payload').get('blob')['displayUrl']}")
-else:
-    print("Image not found on the page.")
+
+def get_raw_album_urls():
+    """
+    returns the raw urls for the hosted images
+    to be used for google image search
+    :return: list of raw urls
+    """
+    urls = get_album_links()
+    raw_urls = []
+    for url in urls:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        scripts = soup.find_all('script')
+        image_tag = list(filter(lambda x: "displayUrl" in str(x), scripts))[0]
+        json_image = json.loads(image_tag.string)
+        if json_image.get('payload').get('blob'):
+            raw_urls.append(json_image.get('payload').get('blob')['displayUrl'])
+    print(raw_urls)
+    return raw_urls
