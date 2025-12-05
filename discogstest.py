5805ec60adc652f768c211faf6e79eda8e531423
Josh Sellers <joshuasellersd@gmail.com>
2025-12-05 00:04:10 -0800

    added another function for runtime

--- discogstest.py
+++ discogstest.py
@@ -34,8 +34,6 @@ def discogs_collection_update(title, url):
     first_result = results[0]
     print(first_result)
 
-    me = d.identity()
-
     file_path = Path(output_file)
 
     if file_path.is_file():
@@ -47,16 +45,6 @@ def discogs_collection_update(title, url):
         with open(file_path, "w") as file:
             file.write(f"Google Result: {title} for URL: {url} */* Discogs Result: {first_result}")
 
-    confirm_additions(output_file)
-    """ Commenting this out for now
-    if first_result.id not in [item.id for item in me.collection_folders[0].releases]:
-        me.collection_folders[0].add_release(first_result.id)
-        print("added to collection")
-    """
-    print("Collection:")
-    for item in me.collection_folders[0].releases:
-        print(item)
-
 
 def confirm_additions(file):
     # TODO - test new function
@@ -66,6 +54,8 @@ def confirm_additions(file):
     with open(file, 'r') as f:
         for line in f:
             content = line.split("*/*")
+            content[0].replace("Google Result: ", "")
+            content[1].replace(" Discogs Result: ", "")
             print(f"For the input {content[0]}, this script got this result from Discogs: {content[1]}")
             new_discog_input = input("Edit the Discog result (blank for no change): ")
             results = d.search(new_discog_input, type='release')
@@ -80,11 +70,32 @@ def confirm_additions(file):
             f.write(line[0] + " */* " + line[1] + "\n")
 
 
+def update_collection():
+    # TODO: test function
+    d = discogs_client.Client('VinylImageReadingProject/0.1', user_token=tokens.discogs_token)
+    me = d.identity()
+    
+    with open(output_file, 'r') as f:
+        for line in f:
+            content = line.split("*/*")
+            content[0].replace("Google Result: ", "")
+            content[1].replace(" Discogs Result: ", "")
+            result = json.loads(content[1])
+            if result not in [item.id for item in me.collection_folders[0].releases]:
+                me.collection_folders[0].add_release(result.id)
+                print("added to collection")
+
+    print("Collection:")
+    for item in me.collection_folders[0].releases:
+        print(item)
+
+
 def main():
     urls = imageUrl.get_raw_album_urls()
     for url in urls:
         title = google_search(url)
         discogs_collection_update(title, url)
+    confirm_additions(output_file)
 
 
 if __name__ == '__main__':
